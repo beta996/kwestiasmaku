d2b495a8d8ebec9f02d316ccc9447aa890ba9974
Knut Zuidema <me@knutzuidema.de>
2022-11-02 12:42:49 +0100

    dictionary created for storing link for ingredients

--- main.py
+++ main.py
@@ -3,6 +3,4 @@ import os
 from web_server import app
 
 if __name__ == '__main__':
-
-
-    app.run(host='0.0.0.0',port=int(os.getenv('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
